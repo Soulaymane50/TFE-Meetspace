3586f99a848dadd17b5c12160c7f3372449304f4
Soulaymane <sou.ezzahti@hotmail.com>
2026-04-02 22:27:08 +0200

    ajoute base spring boot

---
+++ meetspace-backend/src/main/java/be/meetspace/MeetSpaceApplication.java
@@ -0,0 +1,14 @@
+package be.meetspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetSpaceApplication.class, args);
+    }
+
+}
+
---
+++ meetspace-backend/src/test/java/be/meetspace/MeetSpaceApplicationTests.java
@@ -0,0 +1,14 @@
+package be.meetspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
